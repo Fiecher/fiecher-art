a69f0347537f25d10514971cd7a089865d454159
Fiecher <stepanbelebezev@gmail.com>
2026-02-26 21:19:56 +0300

    chore: change linter to antfu/eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,39 @@
-import prettier from 'eslint-config-prettier';
-import path from 'node:path';
-import { includeIgnoreFile } from '@eslint/compat';
-import js from '@eslint/js';
-import svelte from 'eslint-plugin-svelte';
-import { defineConfig } from 'eslint/config';
-import globals from 'globals';
-import ts from 'typescript-eslint';
-import svelteConfig from './svelte.config.js';
+import antfu from '@antfu/eslint-config'
 
-const gitignorePath = path.resolve(import.meta.dirname, '.gitignore');
-
-export default defineConfig(
-	includeIgnoreFile(gitignorePath),
-	js.configs.recommended,
-	...ts.configs.recommended,
-	...svelte.configs.recommended,
-	prettier,
-	...svelte.configs.prettier,
-	{
-		languageOptions: { globals: { ...globals.browser, ...globals.node } },
-		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
-		}
-	},
-	{
-		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
-		languageOptions: {
-			parserOptions: {
-				projectService: true,
-				extraFileExtensions: ['.svelte'],
-				parser: ts.parser,
-				svelteConfig
-			}
-		}
-	}
-);
+export default antfu({
+  typescript: true,
+  svelte: true,
+  ignores: [
+    '**/*.md',
+  ],
+  rules: {
+    'eol-last': ['error', 'always'],
+    'style/operator-linebreak': ['warn', 'after'],
+    'unicorn/prefer-node-protocol': 'off',
+    'no-unused-vars': 'warn',
+    'prefer-const': 'error',
+    'no-console': 'off',
+    'jsonc/sort-keys': 'off',
+    '@typescript-eslint/no-explicit-any': 'error',
+    'unused-imports/no-unused-imports': 'warn',
+    'perfectionist/sort-imports': ['error', {
+      groups: [
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling', 'index'],
+        'type',
+        'side-effect',
+      ],
+      newlinesBetween: 'ignore',
+      order: 'asc',
+      type: 'natural',
+    }],
+  },
+  stylistic: {
+    overrides: {
+      'style/brace-style': ['warn', '1tbs'],
+      'style/arrow-parens': 'off',
+    },
+  },
+})
