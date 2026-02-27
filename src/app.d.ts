4dcd5bcbb3dcd1ae1cb9e53f06b15631f2473b0a
Fiecher <stepanbelebezev@gmail.com>
2026-02-27 14:17:56 +0300

    feat: add config.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,22 +1,13 @@
-import type { FilmStripConfig } from '$lib/types'
+// See https://svelte.dev/docs/kit/types#app.d.ts
+// for information about these interfaces
+declare global {
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
+}
 
-export const APP_CONFIG = {
-  name: 'fiecher-art',
-  version: '0.0.1',
-  author: {
-    name: 'Stepan Belebezev',
-    email: 'your@mom.no',
-  },
-  social: {
-    email: 'mailto:your@mom.no',
-    telegram: 'https://t.me/yourmom',
-    github: 'https://github.com/yourmom',
-  },
-  filmStrip: {
-    cellsPerRow: 3,
-    rows: 2,
-    maxWidth: '480px',
-  } satisfies Partial<FilmStripConfig>,
-} as const
-
-export default APP_CONFIG
+export {}
---
+++ src/lib/config.ts
@@ -0,0 +1,27 @@
+// src/lib/config.ts
+import type { FilmStripConfig } from '$lib/types'
+
+export const APP_CONFIG = {
+  name: 'fiecher-art',
+  version: '0.0.1',
+  author: {
+    name: 'Stepan Belebezev',
+    email: 'your@mom.no',
+  },
+  social: {
+    email: 'mailto:your@mom.no',
+    telegram: 'https://t.me/yourmom',
+    github: 'https://github.com/yourmom',
+    youtube: 'https://youtube.com/@yourmom',
+    artstation: 'https://artstation.com/yourmom',
+    telegramChannel: 'https://t.me/yourmomchannel',
+    behance: 'https://behance.net/yourmom',
+  },
+//   filmStrip: {
+//     cellsPerRow: 3,
+//     rows: 2,
+//     maxWidth: '480px',
+//   } satisfies Partial<FilmStripConfig>,
+} as const
+
+export default APP_CONFIG
